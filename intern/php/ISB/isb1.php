6d7e1afd0e91ec8690204582227f0633f7918a15
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2023-07-30 16:59:29 +0000

    isb1

---
+++ intern/php/ISB/isb1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IRB 1 - Familiarisation with the School Environment and Documents/Records</title>
+    <link rel="stylesheet" href="../../styles/isb1.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+    <h1>IRB 1</h1>
+    <h2>Familiarisation with the School Environment and Documents/Records</h2>
+    <p>Instruction: Complete this observation guide with your mentor. IRB 1 should be completed first week of the internship</p>
+    <form action="" method="post">
+        <label>When was the school established?</label>
+        <input type="year" name="established_year" required><br>
+
+        <label>What is the total number of students in the school?</label>
+        <input type="number" name="total_students" required><br>
+
+        <label>How many are boys?</label>
+        <input type="number" name="boys_students" required><br>
+
+        <label>How many are girls?</label>
+        <input type="number" name="girls_students" required><br>
+
+        <label>What is the number of female teachers in the school?</label>
+        <input type="number" name="female_teachers" required><br>
+
+        <label>What is the number of male teachers in the school?</label>
+        <input type="number" name="male_teachers" required><br>
+
+        <label>How many non-teaching staff (if any) are males?</label>
+        <input type="number" name="male_non_teaching_staff" required><br>
+
+        <label>How many non-teaching staff (if any) are females?</label>
+        <input type="number" name="female_non_teaching_staff" required><br>
+
+        <label>What is the number of classrooms in the school?</label>
+        <input type="number" name="classrooms" required><br>
+
+        <label>What is the reporting time of the school?</label>
+        <input type="time" name="reporting_time" required><br>
+
+        <label>What is the closing time of the school?</label>
+        <input type="time" name="closing_time" required><br>
+
+        <p>Complete the checklist of the following items (Available, Not Available)</p>
+        <label>Sexual Harassment Policy</label>
+        <input type="radio" name="sexual_harassment_policy" value="Available" required> Available
+        <input type="radio" name="sexual_harassment_policy" value="Not Available" required> Not Available<br>
+
+        <label>14. National Gender Policy</label>
+        <input type="radio" name="national_gender_policy" value="Available" required> Available
+        <input type="radio" name="national_gender_policy" value="Not Available" required> Not Available<br>
+
+        <label>15. Equity and Inclusive Education Policy</label>
+        <input type="radio" name="equity_inclusive_policy" value="Available" required> Available
+        <input type="radio" name="equity_inclusive_policy" value="Not Available" required> Not Available<br>
+
+        <!-- Add more checklist items as needed -->
+
+        <label>20. ICT Laboratory</label>
+        <input type="radio" name="ict_laboratory" value="Available" required> Available
+        <input type="radio" name="ict_laboratory" value="Not Available" required> Not Available<br>
+
+        <label>Date of Filling form:</label>
+        <input type="date" name="filling_date" value="<?php echo date('Y-m-d'); ?>" required><br>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
