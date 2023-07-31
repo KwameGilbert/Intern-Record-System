5af8b47b28eee2015d8789b876d49f4fa0563e4d
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2023-07-31 11:17:31 +0000

    modified:   intern/php/ISB/isb1.php modified:   intern/styles/isb.css

--- intern/php/ISB/isb1.php
+++ intern/php/ISB/isb1.php
@@ -9,7 +9,7 @@
     <h2>Familiarisation with the School Environment and Documents/Records</h2>
     
     <form action="" method="post">
-    <h4>Instruction: Complete this observation guide with your mentor. <br>IRB 1 should be completed first week of the internship</h4>
+    <h3>Instruction: Complete this observation guide with your mentor. <br>IRB 1 should be completed first week of the internship</h3>
         <label>When was the school established?</label>
         <input type="year" name="established_year" required><br>
 
@@ -43,25 +43,29 @@
         <label>What is the closing time of the school?</label>
         <input type="time" name="closing_time" required><br>
 
-        <h4>Complete the checklist of the following items (Available, Not Available)</h4>
-        <label>Sexual Harassment Policy</label>
-        <input type="radio" name="sexual_harassment_policy" value="Available" required> Available
-        <input type="radio" name="sexual_harassment_policy" value="Not Available" required> Not Available<br>
 
-        <label>14. National Gender Policy</label>
-        <input type="radio" name="national_gender_policy" value="Available" required> Available
-        <input type="radio" name="national_gender_policy" value="Not Available" required> Not Available<br>
-
-        <label>15. Equity and Inclusive Education Policy</label>
-        <input type="radio" name="equity_inclusive_policy" value="Available" required> Available
-        <input type="radio" name="equity_inclusive_policy" value="Not Available" required> Not Available<br>
-
+        <h3>Complete the checklist of the following items (Available, Not Available)</h3>
+        <div class="radio_q">
+            <label >Sexual Harassment Policy</label>
+            <input type="radio" name="sexual_harassment_policy" value="Available" required> Available
+            <input type="radio" name="sexual_harassment_policy" value="Not Available" required> Not Available<br>
+        </div>
+        <div class="radio_q">
+            <label >National Gender Policy</label>
+            <input type="radio" name="national_gender_policy" value="Available" required> Available
+            <input type="radio" name="national_gender_policy" value="Not Available" required> Not Available<br>
+        </div>
+        <div class="radio_q">
+            <label >Equity and Inclusive Education Policy</label>
+            <input type="radio" name="equity_inclusive_policy" value="Available" required> Available
+            <input type="radio" name="equity_inclusive_policy" value="Not Available" required> Not Available<br>
+        </div>
         <!-- Add more checklist items as needed -->
-
-        <label>20. ICT Laboratory</label>
-        <input type="radio" name="ict_laboratory" value="Available" required> Available
-        <input type="radio" name="ict_laboratory" value="Not Available" required> Not Available<br>
-
+        <div class="radio_q">
+            <label >ICT Laboratory</label>
+            <input type="radio" name="ict_laboratory" value="Available" required> Available
+            <input type="radio" name="ict_laboratory" value="Not Available" required> Not Available<br>
+        </div>
         <label>Date of Filling form:</label>
         <input type="date" name="filling_date" value="<?php echo date('Y-m-d'); ?>" required><br>
 
